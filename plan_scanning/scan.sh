c74787a385226ef7bccf1d1c0d80c8404925328d
zomarg <grammz.k@gmail.com>
2022-01-13 12:17:50 +0100

    Skip checks inline

--- plan_scanning/scan.sh
+++ plan_scanning/scan.sh
@@ -1,3 +1,3 @@
 terraform plan --out ./main.tfplan
 terraform show -json main.tfplan > ./main.json
-checkov -f ./main.json --external-checks-dir ../policies/
+checkov -f ./main.json --external-checks-dir ../policies/ --skip-check CKV2_AZURE_1 CKV2_AZURE_18
