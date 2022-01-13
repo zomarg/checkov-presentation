6ca8d59c8c378d4accbe0b41117d64c4e531ad17
zomarg <grammz.k@gmail.com>
2022-01-13 12:27:20 +0100

    Note for no spaces in skip checks
    Add Commented BusinessOwner in additional_tags

--- plan_scanning/scan.sh
+++ plan_scanning/scan.sh
@@ -1,3 +1,5 @@
 terraform plan --out ./main.tfplan
 terraform show -json main.tfplan > ./main.json
-checkov -f ./main.json --external-checks-dir ../policies/ --skip-check "CKV2_AZURE_1, CKV2_AZURE_18"
+
+#Note no spaces inside skip checks or it wont work
+checkov -f ./main.json --external-checks-dir ../policies/ --skip-check "CKV2_AZURE_1,CKV2_AZURE_18"
